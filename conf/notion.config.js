f641cc70cca9b3adeb9610796fdeb86f7277f90c
2132141424 <2132141424@users.noreply.github.com>
2026-08-10 14:49:27 +0800

    fix: Author属性名映射使用大写以匹配Notion列名

--- conf/notion.config.js
+++ conf/notion.config.js
@@ -36,7 +36,7 @@ module.exports = {
     tags: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TAGS || 'tags',
     icon: process.env.NEXT_PUBLIC_NOTION_PROPERTY_ICON || 'icon',
     ext: process.env.NEXT_PUBLIC_NOTION_PROPERTY_EXT || 'ext', // 扩展字段，存放json-string，用于复杂业务
-    author: process.env.NEXT_PUBLIC_NOTION_PROPERTY_AUTHOR || 'author' // 文章作者，在Notion数据库添加Author属性，每篇文章可指定不同作者
+    author: process.env.NEXT_PUBLIC_NOTION_PROPERTY_AUTHOR || 'Author' // 文章作者，在Notion数据库添加Author属性，每篇文章可指定不同作者
   },
   NOTION_ACTIVE_USER: process.env.NOTION_ACTIVE_USER || '',
   NOTION_TOKEN_V2: process.env.NOTION_TOKEN_V2 || '' // Useful if you prefer not to make your database public
