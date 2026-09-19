1c2dfd288e9e9e7332ef68f3c64d3e83b346e849
2132141424 <2132141424@users.noreply.github.com>
2026-09-19 09:24:04 +0800

    fix: remove announcement rendering and clear notice bar to stop leaking into search snippets

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -92,9 +92,6 @@ export function InfoCard(props) {
 
       <h2 className='text-3xl font-extrabold mt-3'>{siteConfig('AUTHOR')}</h2>
 
-      {/* 公告栏 */}
-      <Announcement post={notice} style={{ color: 'white !important' }} />
-
       <div className='flex justify-between'>
         <div className='flex space-x-3  hover:text-black dark:hover:text-white'>
           {/* 社交按钮 */}
